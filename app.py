3a5c2055924623b717ac99e72152f94316d1a7b2
Gordon Shieh <gshieh@sfu.ca>
2017-06-25 12:32:08 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,74 @@
+import sqlite3
+import itertools
+import urllib
+import requests
+import tinder
+import threading
+import pymysql
+import pymysql.cursors
+
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+conn = pymysql.connect(host="172.17.0.2", user="root", password="qazwsx", database="test", cursorclass=pymysql.cursors.DictCursor)
+
+drop = "DROP TABLE IF EXISTS images; DROP TABLE IF EXISTS users"
+create_query = "CREATE TABLE users(user_id int primary key auto_increment, name varchar(256) UNIQUE)"
+create_img_table_query = "CREATE TABLE images(img_id int primary key auto_increment, user_id int, img_url varchar(256), happy DOUBLE, neutral DOUBLE, sad DOUBLE, FOREIGN KEY(user_id) REFERENCES users(user_id))"
+# with conn.cursor() as cursor:
+#     cursor.execute(drop)
+#     cursor.execute(create_query)
+#     cursor.execute(create_img_table_query)
+
+
+params = {
+    "redirect_uri" : "http://localhost:5000/login/100006055697656",
+    "state" : '{"challenge":"q1WMwhvSfbWHvd8xz5PT6lk6eoA%3D","0_auth_logger_id":"54783C22-558A-4E54-A1EE-BB9E357CC11F","com.facebook.sdk_client_state":true,"3_method":"sfvc_auth"}',
+    "scope" : "user_birthday,user_photos,user_education_history,email,user_relationship_details,user_friends,user_work_history,user_likes",
+    "response_type" : "token,signed_request",
+    "default_audience" : "friends",
+    "return_scopes" : "true",
+    "auth_type" : "rerequest",
+    "client_id" : "464891386855067",
+    "ret" : "login",
+    "sdk" : "ios", 
+    "logger_id" : "54783C22-558A-4E54-A1EE-BB9E357CC11F"
+}
+url = "www.facebook.com/v2.6/dialog/oauth?"
+
+fb_login_url = url + urllib.urlencode(params)
+print(fb_login_url)
+# exit()
+
+@app.route("/login/<int:profile_id>")
+def login(profile_id):
+    print(request.args)
+    token = request.args.get("access_token")
+    print(token)
+    if token:
+        t = threading.Thread(target=tinder.scrape, args=(conn, profile_id, token))
+        t.start()
+
+    return "authorized!"
+
+@app.route("/")
+@app.route("/<int:page>")
+def template_test(page=1):
+    cursor = conn.cursor()
+    print (((page * 12) - 12, page * 12))
+
+    x = cursor.execute("""
+        select u.name, i.img_url, i.happy, i.neutral, i.sad from images i 
+        JOIN users u ON i.user_id = u.user_id limit 12 OFFSET %s
+        """, (page * 12,))
+    l = cursor.fetchall()
+    for item in l:
+        item['happy'] = round(item['happy'], 4)
+        item['neutral'] = round(item['neutral'], 4)
+        item['sad'] = round(item['sad'], 4)
+    return render_template('templates.html', my_string="Tinder Surprise", my_list=l)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tinder.py
@@ -0,0 +1,51 @@
+import requests
+import pynder
+import json
+import itertools
+import os
+
+login = "https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&state=%7B%22challenge%22%3A%22q1WMwhvSfbWHvd8xz5PT6lk6eoA%253D%22%2C%220_auth_logger_id%22%3A%2254783C22-558A-4E54-A1EE-BB9E357CC11F%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=54783C22-558A-4E54-A1EE-BB9E357CC11F#_=_"
+# Female (Tinas) Account
+# facebook_id = "100006055697656"
+# token = "EAAGm0PX4ZCpsBAPzV5Krz8VTbA8SqrS71WbND98yySwdHBx2WzesNw4X58nVk01ZBN84osVZAZCM9TxiZBwUzgziePn0aNtVR4bZAfr3QiUYV0ecTdSCDVpIZBgiHHXrEFIJTo2Vb3edEYZBdXTNB8lWbd71w2iUu1kimp7L4S75Qvv6yqxtvv0WY7QbZCcpp8DhNriiv1QFR0pInjZBu8c3JJIm9OzGIYNvVJTUIQMmwRuTbrqUFKqXJxcgHKU64kodjZB3ZAi5BTBN60IgR7dJ6NWR"
+
+
+endpoint = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize"
+sub_key = "8b6fa03beb9f4d58a384dbcbe1e48d77"
+
+def emotion(link):
+    req = requests.post(url=endpoint, json={"url": link },
+                   headers={'Content-Type': 'application/json',
+                   'Ocp-Apim-Subscription-Key': sub_key})
+
+    return req.json()[0]
+
+
+
+def scrape(conn, facebook_id, token):
+    msession = pynder.Session(facebook_id=facebook_id, facebook_token=token)
+    msession.update_location(47.615699768066, -102.344497680661191781)
+    cursor = conn.cursor()
+    for item in msession.nearby_users():
+        msession.update_location(47.615699768066, -102.344497680661191781)
+        print(item.name)
+        try:
+            cursor.execute("insert into users(name) values(%s)", (item.name,))
+            for img_url in item.get_photos(width="640"):
+                print(img_url)
+                scores = emotion(img_url)["scores"]
+                happy = scores["surprise"] + scores["happiness"]
+                neutral = scores["neutral"]
+                sad = scores["sadness"]
+                cursor.execute("insert into images(user_id, img_url, happy, neutral, sad) values ((select max(user_id) from users), %s, %s, %s, %s)", (str(img_url), happy, neutral, sad,))
+                conn.commit()
+
+                break; # feeling lazy
+        except:
+            pass
+
+     
+# x = cursor.execute("SELECT distinct u.name as name, i.img_url as img_url, distinct i.emotions as emotions FROM images i join users u")   
+# l = [dict(itertools.izip(row.keys(), row)) for row in x.fetchall()]
+
+# print(l[0])
